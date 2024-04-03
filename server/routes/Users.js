66029911759327959dd27feeff2ef99c5f47fb66
Dang Nguyen Tien Hau <tienhau.it@gmail.com>
2024-04-03 11:59:47 +0700

    Push again

--- server/routes/Users.js
+++ server/routes/Users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-// const {validToken} = require('../middlewares/AuthenMiddlewares');
+const {validToken} = require('../middlewares/AuthenMiddlewares');
 const UsersController = require('../controllers/UsersController')
 
 router.get('/', (req, res) => {res.send('Users page')});
